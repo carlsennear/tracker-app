33b98de192499416f882c12bdea2843a6bf0bc2c
Арговестa <124946616+carlsennear@users.noreply.github.com>
2025-06-11 16:01:06 +0700

    model.js

--- model.js
+++ model.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 const logSchema = new mongoose.Schema({
-  latitude: Number,
-  longitude: Number,
+  latitude: String,
+  longitude: String,
   photo: String,
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Log", logSchema);
